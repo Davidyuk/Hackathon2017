e5387d91d90bfad59a912aa97508895bcad14109
Max <acmxca@gmail.com>
2017-04-10 05:32:51 +1000

    Complete cars page

--- app/client/templates/views/cars.js
+++ app/client/templates/views/cars.js
@@ -5,50 +5,62 @@ Template['views_cars'].onCreated(function() {
 
 Template['views_cars'].onRendered(function() {
     template = this;
-    var autoId = FlowRouter.getParam('id');
+    let autoId = FlowRouter.getParam('id');
 
     if (web3.isAddress(autoId)) {
-        var auto = web3.eth.contract(Meteor.ethContcactConfig.autoAbi).at(autoId);
+        let auto = web3.eth.contract(Meteor.ethContcactConfig.autoAbi).at(autoId);
 
-        var l = auto.getOperationsLength().toNumber();
+        let l = auto.getOperationsLength().toNumber();
         console.log(l);
-
         console.log(auto.owner());
         console.log(auto.registry());
         console.log(auto.hidden());
 
-
         TemplateVar.set(template, 'loading', true);
-        var names = [];
-        var dates = [];
-        var comments = [];
-        var services = [];
-        for (var i = (l-1); i >= 0; i--) {
-            var service = new Object();
-            var o = auto.operations(i);
+
+        function formatDate(val){
+            return val < 10 ? '0' + val : val;
+        }
+
+        let autoInfo = [{
+            owner: auto.owner(),
+            autoId: auto.id(),
+            historyShowed: auto.hidden() ? 'Да' : 'Нет'
+        }];
+
+        let services = [];
+        for (let i = (l-1); i >= 0; i--) {
+            let service = {};
+            let o = auto.operations(i);
             o[0] = o[0].toNumber();
-            o[4] = o[4].toNumber();
-            var r = web3.eth.contract(Meteor.ethContcactConfig.registryAbi).at(o[1]);
+
+            let seconds = o[4].toNumber() * 1000;
+            let date = new Date(seconds);
+            o[4] = formatDate(date.getDate()) + '.' +formatDate(date.getMonth()) + '.' + formatDate(date.getFullYear());
+
+            let r = web3.eth.contract(Meteor.ethContcactConfig.registryAbi).at(o[1]);
             o[5] = r.name();
             o[6] = r.operationTypes(o[0]);
-            var p = r.performers(o[2]);
-            // console.log(p);
+            let p = r.performers(o[2]);
+            console.log(p);
             p[1] = p[1].toNumber();
             o[7] = p;
             console.log(o);
 
-            service.name = o[5];
+            service.registry = o[5];
+            service.name = (o[7][0] === '' ? o[2] : o[7][0]);
+            service.rating = o[7][1] === 0 ? '-' : o[7][1];
+            service.service = o[6][0];
             service.date = o[4];
             service.comment = o[3];
+
             services.push(service);
         }
         console.log(services);
+
         TemplateVar.set(template, 'services', services);
-        // setTimeout(function() {
-        //
-        //     TemplateVar.set(template, 'services', [3, 2, 1]);
-        // }, 3000);
         TemplateVar.set(template, 'loading', false);
+        TemplateVar.set(template, 'autos', autoInfo);
     }
 });
 
